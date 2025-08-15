03567a997f7e63630ddbf02b5a3ee1f9d6276264
Pratyush Warungase <140175494+ItsPratyush@users.noreply.github.com>
2025-08-15 12:18:55 +0530

    Add files via upload

--- src/main/java/com/example/demo/TaskController.java
+++ src/main/java/com/example/demo/TaskController.java
@@ -3,50 +3,59 @@ package com.example.demo;
 import com.example.demo.Task;
 
 import java.util.*;
-
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
-	private List<Task> tasks = new ArrayList<>(
-		    List.of(
-		        new Task(1L, "Learn Spring Boot", "Understand REST API basics"),
-		        new Task(2L, "Build Todo App", "Use ArrayList to store tasks")
-		    )
-		);
+//	private List<Task> tasks = new ArrayList<>(
+//		    List.of(
+//		        new Task(1L, "Learn Spring Boot", "Understand REST API basics"),
+//		        new Task(2L, "Build Todo App", "Use ArrayList to store tasks")
+//		    )
+//		);
 	
-	@GetMapping("/tasks")
+	@Autowired
+	private TaskRepository taskrepository;
+	
+	@GetMapping
 	public List<Task> getTask() {
-		return tasks;
+//		return tasks;
+		return taskrepository.findAll();
+		
 	}
 	
-	@PostMapping("/tasks")
-	public List<Task> createTask(@RequestBody Task task) {
-		tasks.add(task);
-		return tasks;
+	@PostMapping
+	public Task createTask(@RequestBody Task task) {
+//		tasks.add(task);
+//		return tasks;
+		return taskrepository.save(task);
 	}
 	
-	@PutMapping("/tasks/{id}")
-	public List<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
-		for(Task t : tasks) {
-			if (t.getId().equals(id)) {
-				t.setTitle(updatedTask.getTitle());
-				t.setDescription(updatedTask.getDescription());
-			}
-		}
-		return tasks;
+	@PutMapping("/{id}")
+	public Task updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+//		for(Task t : tasks) {
+//			if (t.getId().equals(id)) {
+//				t.setTitle(updatedTask.getTitle());
+//				t.setDescription(updatedTask.getDescription());
+//			}
+//		}
+//		return tasks;
+		return taskrepository.findById(id) // find for id
+				.map(task -> { // if found do this
+					task.setTitle(updatedTask.getTitle());
+					task.setDescription(updatedTask.getDescription());
+					return taskrepository.save(task);
+				}) // if not then throw exception
+				.orElseThrow(() -> new RuntimeException("Task not found."));
 	}
 	
-	@DeleteMapping("/tasks/{id}")
-	public List<Task> deleteTask(@PathVariable Long id) {
-		tasks.removeIf(t -> t.getId().equals(id));
-		return tasks;
+	@DeleteMapping("/{id}")
+	public void deleteTask(@PathVariable Long id) {
+//		tasks.removeIf(t -> t.getId().equals(id));
+//		return tasks;
+		taskrepository.deleteById(id);
 	}
 
 }
