23eaa70e24ad5b7e3ac901ed209dd775afe71ca2
Pratyush Warungase <140175494+ItsPratyush@users.noreply.github.com>
2025-08-21 02:05:22 +0530

    Add files via upload

--- src/main/java/com/example/demo/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/CustomUserDetailsService.java
@@ -1,5 +1,8 @@
-package com.example.demo;
-
-public class CustomUserDetailsService {
-
-}
+//package com.example.demo;
+//
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//}
